10af5b0bee218ff9f6b263121ec2c15f36ca1de2
mattearlpinoodev <mattearlpinoo@gmail.com>
2024-08-30 21:28:53 +0800

    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Health Care Management System</title>
+    <title>Health Care Management</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
